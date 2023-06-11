cd106d49b43726b0a5f104dca528873a3e0c0fe2
unknown <rammer94@gmail.com>
2023-06-10 20:36:30 -0500

    correccion del setting.py

--- attendance/settings/local.py
+++ attendance/settings/local.py
@@ -1,14 +1,9 @@
 from .base import *
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -20,10 +15,8 @@ DATABASES = {
     }
 }
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
--- attendance/wsgi.py
+++ attendance/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance.settings.prod')
 
 application = get_wsgi_application()
